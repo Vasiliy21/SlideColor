17e5bffe3f2e574f168c7eaa9b1a4ee6d0790f4a
Vasiliy Startsev <Vasiliystartsev@gmail.com>
2022-06-27 01:07:21 +0300

    fixed up sliders

--- SlideColor/ViewController.swift
+++ SlideColor/ViewController.swift
@@ -9,29 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var RGBView: UIView!
-
+    
     @IBOutlet var redValue: UILabel!
     @IBOutlet var greenValue: UILabel!
     @IBOutlet var blueValue: UILabel!
-
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         RGBView.layer.cornerRadius = 10
-
         setupValues()
     }
-
+    
     @IBAction func slidersAction() {
         redValue.text = "\(round(redSlider.value * 100)/100)"
         greenValue.text = "\(round(greenSlider.value * 100)/100)"
         blueValue.text = "\(round(blueSlider.value * 100)/100)"
         RGBView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
-
+    
     // MARK: - Private Methods
     private func setupValues() {
         redValue.text = String(redSlider.value)
